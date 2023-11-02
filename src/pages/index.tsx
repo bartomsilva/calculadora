1080762470b37b89a6ff6fad55f23fac39bd0c43
bartomsilva <bartomsilva@gmail.com>
2023-11-01 23:47:36 -0300

    atualização texto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,114 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [ultKey, setUltKey] = useState<string>("")
+  const [start, setStart] = useState(true)
+  const [expression, setExpression] = useState<string>("")
+  const [lineUp, setLineUp] = useState<string>("")
+  const [lineDown, setLineDown] = useState<string>("")
+
+  function isSymbol(key: string) {
+    return "+-/*".includes(key)
+  }
+
+  function setValue(key: string) {
+
+    if (isSymbol(key) || !isNaN(+key) && start) {
+      if (!isNaN(+key)) {
+        let newLineUp = lineUp.replace(/[*\-+/]/g, '') + key
+        setLineUp(newLineUp)
+      } else {
+        if (!isSymbol(ultKey)) {
+          setLineUp(key)
+        }
+      }
+    }
+
+    let newExpression=""
+
+    switch (true) {
+      case key == "CE":
+        setExpression("")
+        setLineUp("")
+        setLineDown("")
+        setStart(true)
+        break
+      case (key == "BS"):
+        newExpression = expression.slice(0, -1)
+        setExpression(newExpression)
+        setLineDown(newExpression)
+        if(start){
+          setLineUp(newExpression)
+        } else {
+          setLineUp("")
+        }
+        break
+      case key == "=":
+        if(!isSymbol(lineDown.charAt(lineDown.length-1))){
+          const result = eval(lineDown)
+          setLineUp(result)
+          setStart(false)
+        }
+        break
+      case (key !== "BS"):        
+        newExpression = expression + key
+        setExpression(newExpression)
+        setLineDown(newExpression)
+    }
+    setUltKey(key)
+
+  }
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <main className="calculator">
+      <h1 className='text-3xl mb-3'>Simplizinha - By Bart</h1>
+
+      <div className='display'>
+        <div className='lineUpDisplay bg-zinc-300'>
+          {lineUp}
+        </div>
+        <div className='lineDownDisplay'>
+          {lineDown}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='calcPanel'>
+        <div className="containerNumbers">
+          <button onClick={() => setValue('7')} className='buttonSmall'>7</button>
+          <button onClick={() => setValue('8')} className='buttonSmall'>8</button>
+          <button onClick={() => setValue('9')} className='buttonSmall'>9</button>
+          <button onClick={() => setValue('4')} className='buttonSmall'>4</button>
+          <button onClick={() => setValue('5')} className='buttonSmall'>5</button>
+          <button onClick={() => setValue('6')} className='buttonSmall'>6</button>
+          <button onClick={() => setValue('1')} className='buttonSmall'>1</button>
+          <button onClick={() => setValue('2')} className='buttonSmall'>2</button>
+          <button onClick={() => setValue('3')} className='buttonSmall'>3</button>
+          <button onClick={() => setValue('0')} className='buttonMedium'>0</button>
+          <button onClick={() => setValue('.')} className='buttonSmall'>.</button>
+        </div>
+
+        <div className='containerSymbols'>
+          <button onClick={() => setValue('CE')} className='buttonSmall bg-[#4a8cfa]'> CE</button>
+          <button onClick={() => setValue('BS')} className='buttonSmall bg-[#fa4a4a]'> ◄-</button>
+          <button onClick={() => setValue('/')} className='buttonSmall'>/</button>
+          <button onClick={() => setValue('*')} className='buttonSmall'>*</button>
+          <button onClick={() => setValue('-')} className='buttonSmall'>-</button>
+          <button onClick={() => setValue('+')} className='buttonSmall'>+</button>
+          <button onClick={() => setValue('=')} className='buttonMedium bg-[#F4F622]'>=</button>
+        </div>
+
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
+
+function getValueButton(key: string) {
+
+  console.log(key)
+}
+
